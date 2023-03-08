22619b72b48be6711cd126291c6ef2b51f3bd0f4
Abhishek Adhikari <ad.abhi0013@gmail.com>
2023-03-08 04:43:32 +0000

    Finish authentication

--- blango/settings.py
+++ blango/settings.py
@@ -47,8 +47,16 @@ INSTALLED_APPS = [
     'blog',
     'crispy_forms',
     'crispy_bootstrap5',
-    'rest_framework'
+    'rest_framework',
+    'rest_framework.authtoken'
 ]
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.BasicAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
+        "rest_framework.authentication.TokenAuthentication",
+    ]
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
