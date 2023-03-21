6bc8143b7223ce31b70bd03d951b41ab9cf53b60
Abhishek Adhikari <ad.abhi0013@gmail.com>
2023-03-21 03:59:46 +0000

    Finish viewsets and routers

--- blog/api/urls.py
+++ blog/api/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path, include
 from rest_framework.authtoken import views
+from blog.api.views import UserDetail, TagViewSet, PostViewSet
 from rest_framework.urlpatterns import format_suffix_patterns
+from rest_framework.routers import DefaultRouter
+from blog.api.views import PostList, PostDetail, UserDetail, TagViewSet
 from django.urls import path, include, re_path
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
@@ -18,6 +21,10 @@ schema_view = get_schema_view(
     public=True,
 )
 
+router = DefaultRouter()
+router.register("tags", TagViewSet)
+router.register("posts", PostViewSet)
+
 urlpatterns += [
     path("auth/", include("rest_framework.urls")),
     path("token-auth/", views.obtain_auth_token),
@@ -34,13 +41,16 @@ urlpatterns += [
 ]
 
 urlpatterns = [
-    path("posts/", PostList.as_view(), name="api_post_list"),
-    path("posts/<int:pk>", PostDetail.as_view(), name="api_post_detail"),
     path("users/<str:email>", UserDetail.as_view(), name="api_user_detail"),
 ]
 urlpatterns += [
     path("auth/", include("rest_framework.urls")),
     path("token-auth/", views.obtain_auth_token)
+]
+urlpatterns += [
+    path("auth/", include("rest_framework.urls")),
+    # ... other patterns omitted
+    path("", include(router.urls)),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
